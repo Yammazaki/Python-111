c419797c88feeb305990d562350e5defc4ca7dd2
BuzinAlexey <bess.post@mail.ru>
2022-10-19 16:33:57 +0300

    + функции поиск, просмотр

--- DZ7/controller.py
+++ DZ7/controller.py
@@ -2,14 +2,22 @@ import func
 import view 
 
 def directory():
-    oper = input('Выберите действие: ')
+     oper = int(input('1 - Добавить контакт \n2 - Удалить контакт \n3 - Найти котакт \n4 - Показать все контакты \n5 - Выход \n Введите, что вы хотите сделать 1-5: '))
+     while oper <= 5:
+          if oper == 1:
+               func.get_add()
+          elif oper == 2:
+               func.get_del()
+          elif oper == 3:
+               func.get_find()
+          elif oper == 4:
+               func.get_see()
+          if oper == 5:
+               print('Спасибо, что выбрали наш Продукт')
+               break
+          oper = int(input('1 - Добавить контакт \n 2 - Удалить контакт \n 3 - Найти котакт \n 4 - Показать все контакты \n 5 - Выход \n Введите, что вы хотите сделать 1-5: '))
+            
 
-    if oper == '1':
-         res = func.get_add()
-    # elif oper == '2':
-    #    res = func.get_del()
-    elif oper == '3':
-       res = func.get_find()
-    elif oper == '4':
-         res = func.get_see()
-    return res
+    
+    
+      
--- DZ7/func.py
+++ DZ7/func.py
@@ -6,7 +6,10 @@ def get_add():
     data = view.get_input(list)
     with open('people.csv', 'a', encoding = 'UTF-8') as file:
         file.writelines(
-            f'{data[0]}, {data[1]}, {data[2]}, {data[3]} \n')
+            f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
+    with open('people.txt', 'a', encoding = 'UTF-8') as file:
+        file.writelines(
+            f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
 
 def get_see():
     with open('people.csv', 'r', encoding = 'UTF-8') as file:
@@ -15,37 +18,34 @@ def get_see():
         print(list)
 
 def get_find():
-   
-# def get_find():
-#    with open('people.csv', 'r', encoding = 'UTF-8') as file:
-#         list = []
-#         list = file.readlines()
-#         print(list)
-#         for i in range(len(list)):
-#             list[i] = ''.join(list[i])
-#             list[i] = list[i].replace(',','').split()
-#             req = []
-#             reg = list[i]
-#             print(req)
+   with open('people.csv', 'r', encoding = 'UTF-8') as file:
+        data = []
+        data = file.readlines()
+        text = input('Введите слово для поиска: \n')
+        for i in range(len(data)):
+            data[i] = ''.join(data[i])
+            row = []
+            row = data[i]
+            if text in row:
+                print(row)
+    
 
-#             # text = input('Введите слово для поика: ')
-#             # for i in range(len(list[i])):
-#             #     if text == (list[i])[i]:
-#             #     print(list[i][i],i)
+                
+            
+        
         
         
 
-# def get_find():
-#    with open('people.csv', 'r', encoding = 'UTF-8') as file:
-#         list = []
-#         list = ' '.join(' '.join(file.readlines()).split())
-#         print(list)
-#         list = list.replace(',','').split()
-#         print(list)
-#         text = input('Введите слово для поика: ')
-#         for i in range(len(list)):
-#             if text == list[i]:
-#                 print(list[i],i)
+
+                  
+
+        
+          
+
+           
+        
+        
+
         
         
 
