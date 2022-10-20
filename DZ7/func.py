5d75d400c3c70b8386a802b302cb6166275e7c96
BuzinAlexey <bess.post@mail.ru>
2022-10-20 16:45:39 +0300

    Add func get_add_find

--- DZ7/func.py
+++ DZ7/func.py
@@ -1,4 +1,5 @@
 import view
+import os
 
 def get_add():
     list = ['Фамилию: ','Имя: ','номер телефона: ','коментарий']
@@ -44,10 +45,38 @@ def get_del():
             print(f'№ {i+1} |{data[i]}')
     with open('people.txt', 'w', encoding = 'UTF-8') as file:
         file.writelines(data)
-    with open('people.csv', 'w', encoding = 'UTF-8') as file:
-        file.writelines(data)
+    # with open('people.csv', 'w', encoding = 'UTF-8') as file:
+    #     file.writelines(data)
 
-            
+def get_del_find():
+    with open('people.csv', 'r', encoding = 'UTF-8') as file:
+        data = []
+        data_see = []
+        data = file.readlines()
+        text = input('Введите слово для поиска: \n')
+        for i in range(len(data)):
+            data[i] = ''.join(data[i])
+            row = []
+            row = data[i]
+            if text in row:
+                print(f'N {i+1} удалить? : {row}')
+        oper = int(input('Удалить найденные записи \n1 - да \n2 - нет \nВвод: ')) 
+        if oper == 1: 
+            os.remove('people.txt')       
+            for i in range(len(data)):
+                data[i] = ''.join(data[i])
+                row = []
+                row = data[i]
+                data_new = []
+                if text not in row:        
+                    data_new.append(row)
+                    data_new = ' '.join(' '.join(''.join(data_new).split()).split())
+                with open('people.txt', 'a', encoding = 'UTF-8') as file:
+                    file.writelines(f'{data_new} \n')
+                # with open('people.csv', 'a', encoding = 'UTF-8') as file:
+                #     file.writelines(data)
+        if oper == 2: 
+            get_see() 
      
 
    
