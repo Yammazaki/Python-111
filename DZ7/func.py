5c9086c308c949bf9ebabd31c6fb977c419ba81c
BuzinAlexey <bess.post@mail.ru>
2022-10-20 20:08:08 +0300

    Phone directory ver 1.1

--- DZ7/func.py
+++ DZ7/func.py
@@ -45,14 +45,15 @@ def get_del():
             print(f'№ {i+1} |{data[i]}')
     with open('people.txt', 'w', encoding = 'UTF-8') as file:
         file.writelines(data)
-    # with open('people.csv', 'w', encoding = 'UTF-8') as file:
-    #     file.writelines(data)
+    with open('people.csv', 'w', encoding = 'UTF-8') as file:
+        file.writelines(data)
 
 def get_del_find():
     with open('people.csv', 'r', encoding = 'UTF-8') as file:
         data = []
         data_see = []
         data = file.readlines()
+        data_new = []
         text = input('Введите слово для поиска: \n')
         for i in range(len(data)):
             data[i] = ''.join(data[i])
@@ -60,23 +61,20 @@ def get_del_find():
             row = data[i]
             if text in row:
                 print(f'N {i+1} удалить? : {row}')
-        oper = int(input('Удалить найденные записи \n1 - да \n2 - нет \nВвод: ')) 
+                data_new.append(row)     
+        oper = int(input('Удалить найденные записи \n1 - да \n2 - нет(вывести список контактов) \nВвод: ')) 
         if oper == 1: 
-            os.remove('people.txt')       
-            for i in range(len(data)):
-                data[i] = ''.join(data[i])
-                row = []
-                row = data[i]
-                data_new = []
-                if text not in row:        
-                    data_new.append(row)
-                    data_new = ' '.join(' '.join(''.join(data_new).split()).split())
-                with open('people.txt', 'a', encoding = 'UTF-8') as file:
-                    file.writelines(f'{data_new} \n')
-                # with open('people.csv', 'a', encoding = 'UTF-8') as file:
-                #     file.writelines(data)
+            data_del = comp(data,data_new)
+            data_del = ''.join(data_del)
+        with open('people.txt', 'w', encoding = 'UTF-8') as file:
+            file.writelines(f'{data_del} \n')
+        with open('people.csv', 'w', encoding = 'UTF-8') as file:
+            file.writelines(data)
         if oper == 2: 
             get_see() 
+
+def comp(a,b):
+    return list(set(a)-set(b))
      
 
    
