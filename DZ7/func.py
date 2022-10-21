1645b5fe830a677f8b0962e4b2a4fb65886d5f76
BuzinAlexey <bess.post@mail.ru>
2022-10-21 09:55:42 +0300

    Optimization PhoneBook

--- DZ7/func.py
+++ DZ7/func.py
@@ -7,15 +7,12 @@ def get_add():
     with open('people.csv', 'a', encoding = 'UTF-8') as file:
         file.writelines(
             f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
-    with open('people.txt', 'a', encoding = 'UTF-8') as file:
-        file.writelines(
-            f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
 
 def get_see():
     with open('people.csv', 'r', encoding = 'UTF-8') as file:
-        list = []
-        list = ''.join(file.readlines())
-        print(list)
+        data = []
+        data = file.readlines()
+        print(''.join(data))
 
 def get_find():
     with open('people.csv', 'r', encoding = 'UTF-8') as file:
@@ -28,25 +25,29 @@ def get_find():
             row = []
             row = data[i]
             if text in row:
-                print(f'N {i+1} : {row}')
+                print(f'№ {i+1} : {row}')
         
+
 def get_del():
     with open('people.csv', 'r', encoding = 'UTF-8') as file:
         data = []
         data = file.readlines()
         print('-'*100)
         for i in range(len(data)):
-            print(f'№ {i+1} |{data[i]}')
-    number = int(input('Введите номер контакта для удаления: '))
-    n = number-1
-    data.pop(n)
-    print('Вы удалили контакт')
-    for i in range(len(data)):
-            print(f'№ {i+1} |{data[i]}')
-    with open('people.txt', 'w', encoding = 'UTF-8') as file:
-        file.writelines(data)
-    with open('people.csv', 'w', encoding = 'UTF-8') as file:
-        file.writelines(data)
+            print(f'№ {i+1} : {data[i]}')
+        oper = int(input('Удалить контакт \n1 - да \n2 - нет \nВвод: ')) 
+        if oper == 1: 
+            number = int(input('Введите номер контакта для удаления: '))
+            n = number-1
+            data.pop(n)
+            data = ''.join(data)
+            print(f'Вы удалили контакт № {number}')
+            with open('people.csv', 'w', encoding = 'UTF-8') as file:
+                file.writelines(f'{data} \n')
+        elif oper == 2: 
+            False
+        else:
+            False    
 
 def get_del_find():
     with open('people.csv', 'r', encoding = 'UTF-8') as file:
@@ -62,16 +63,16 @@ def get_del_find():
             if text in row:
                 print(f'N {i+1} удалить? : {row}')
                 data_new.append(row)     
-        oper = int(input('Удалить найденные записи \n1 - да \n2 - нет(вывести список контактов) \nВвод: ')) 
+        oper = int(input('Удалить найденные записи \n1 - да \n2 - нет \nВвод: ')) 
         if oper == 1: 
             data_del = comp(data,data_new)
             data_del = ''.join(data_del)
-        with open('people.txt', 'w', encoding = 'UTF-8') as file:
-            file.writelines(f'{data_del} \n')
-        with open('people.csv', 'w', encoding = 'UTF-8') as file:
-            file.writelines(data)
-        if oper == 2: 
-            get_see() 
+            with open('people.csv', 'w', encoding = 'UTF-8') as file:
+                file.writelines(f'{data_del} \n')
+        elif oper == 2: 
+            False
+        else:
+            False
 
 def comp(a,b):
     return list(set(a)-set(b))
