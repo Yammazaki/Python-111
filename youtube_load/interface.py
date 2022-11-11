90f06dd30e8ead1dc3cd6c20c9db0259d238caf0
BuzinAlexey <bess.post@mail.ru>
2022-11-11 21:41:23 +0300

    YT loader 2.0

--- youtube_load/interface.py
+++ youtube_load/interface.py
@@ -1,6 +1,7 @@
 import tkinter
 from tkinter import *
 import pytube
+from PIL import Image, ImageTk
 
 def load():
     video_link = Entry.clipboard_get(enter_link)
@@ -18,20 +19,27 @@ def start():
     global enter_save
     
     window = Tk()
-    window.title('загрузка')
-    window.geometry('400x240')
+    window.title('Downloader YouTube 1.0')
+    window.geometry('380x380')
     window.columnconfigure(index=0, weight=50)
     window.columnconfigure(index=1, weight=250)
+
+    canvas = tkinter.Canvas(window, height=225, width=225)
+    image = Image.open("E:\учеба\Python\youtube_load\you1.jpg")
+    photo = ImageTk.PhotoImage(image)
+    image = canvas.create_image(0, 0, anchor='nw',image=photo)
+    canvas.grid(row=0,columnspan=2)
+    
     link = Label(window, text='Введите ссылку для скачивания')
-    link.grid(row=0, column=0, sticky = 'e')
+    link.grid(row=1, column=0, sticky = 'e')
     save = Label(window, text='Введите путь для сохранения')
-    save.grid(row=1, column=0, sticky = 'e')  
+    save.grid(row=2, column=0, sticky = 'e')  
     enter_link = Entry(window) 
-    enter_link.grid(row=0, column=1, sticky = 'w')
+    enter_link.grid(row=1, column=1, sticky = 'w',pady=10)
     enter_save = Entry(window)  
-    enter_save.grid(row=1, column=1, sticky = 'w')
+    enter_save.grid(row=2, column=1, sticky = 'w',pady=10)
 
     btn = Button(window, text='скачать',command = load)
-    btn.grid(row=4, columnspan=2)
+    btn.grid(row=3, columnspan=2, pady=10)
     window.mainloop()
 
